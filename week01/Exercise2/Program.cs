82e25e6d08c37463f3463f9cf7392a9041bb852e
yourusername <temaneke@byupathway.edu>
2026-07-04 16:56:52 +0200

    exercise 1 and exercise2 completed

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade? ");
+        string answer = Console.ReadLine();
+        int grade = int.Parse(answer);
+        string finalGrade = "";
+        
+        if (grade >= 90)
+        {
+            finalGrade = "A";  
+        }
+        else if (grade >= 80)
+        {
+            finalGrade = "B";
+        }
+        else if (grade >= 70)
+        {
+            finalGrade = "C";
+        
+        }
+        else if (grade >= 60)
+        {
+            finalGrade = "D";
+        }
+        else
+        {
+            finalGrade = "F";
+        }
+        Console.WriteLine($"You had an {finalGrade}.");
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulation on making it!");
+        }
+        else
+        {
+            Console.WriteLine("Please don't be discouraged. Try again next semester!");
+        }
+
     }
 }
