e9dedc665cacead9eb595402024a46c1f6437edf
yourusername <temaneke@byupathway.edu>
2026-07-04 18:55:33 +0200

    completed exercise 3 with all of the stretch assignment.

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,38 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        Random randomGenerator = new Random();
+        string playAgain = "yes"; 
+        while (playAgain == "yes")
+        {
+        int mysteriousNumber = randomGenerator.Next(1, 101);
+        int guess;
+        int attemptNumber = 0;
+        do 
+        {
+        Console.Write("Give your guess: ");
+        string answer = Console.ReadLine();
+        guess = int.Parse(answer);
+        if (guess > mysteriousNumber)
+        {
+            Console.WriteLine("Lower");
+            attemptNumber ++;
+        }
+        else if (guess < mysteriousNumber)
+        {
+            Console.WriteLine("Higher");
+            attemptNumber ++;
+        }
+        else
+        {
+            Console.WriteLine("Congratulations! You guessed it.");
+            Console.WriteLine($"It took you {attemptNumber} tries to get it.");
+            Console.Write("Do you want to play again? ");
+            playAgain = Console.ReadLine();
+        }
+        } 
+        while (guess != mysteriousNumber);
+        }
+
     }
 }
